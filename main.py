5f565697d573020c0fe9cf2df2eb2594182d7392
nicolektang <137560389+nicolektang@users.noreply.github.com>
2024-04-30 16:57:31 -0700

    test

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import streamlit as st
-import matplotlib
-
-# Specify the backend explicitly
-matplotlib.use("Agg")  # Use the Agg backend
-
 import matplotlib.pyplot as plt
 import numpy as np
 
-
+# Specify the backend explicitly
+import matplotlib
+matplotlib.use("Agg")  # Use the Agg backend
 
 foodItem = st.text_input("Enter Food Type")
 
@@ -15,19 +12,18 @@ myDict = {1: "one", 2: "two", 3: "three", 4: "four"}
 
 optionsList = list(myDict.keys())
 
-
 option = st.selectbox(
     'How would you like to be contacted?',
     optionsList)
 
 st.write('You selected Series ID:', option)
-st.write("You selected Food Item: ", optionsList[option])
+st.write("You selected Food Item: ", myDict.get(option, "Not found"))
 
-
-st.image('testing.png', caption = "testing stuff")
+st.image('testing.png', caption="testing stuff")
 
 arr = np.random.normal(1, 1, size=100)
 fig, ax = plt.subplots()
 ax.hist(arr, bins=20)
 
 st.pyplot(fig)
+
