75f85e749009810a7058d1778f0b8bf3c771cc5b
nicolektang <137560389+nicolektang@users.noreply.github.com>
2024-04-30 16:51:29 -0700

    test

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
 
-with st.sidebar:
 
-    # make widgets 
-    car_name = st.text_input('Car Name', 'optional')
+foodItem = st.text_input("Enter Food Type")
+
+myDict = {1: "one", 2: "two", 3: "three", 4: "four"}
+
+optionsList = list(myDict.keys())
+
+
+option = st.selectbox(
+    'How would you like to be contacted?',
+    optionsList)
+
+st.write('You selected Series ID:', option)
+st.write("You selected Food Item: ", optionsList[option])
+
+
+st.imapge('testing.png', caption = "testing stuff")
+
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+st.pyplot(fig)
