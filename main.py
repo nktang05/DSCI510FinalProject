76fd107c5e81fd39882844694388713350d20952
nicolektang <137560389+nicolektang@users.noreply.github.com>
2024-04-30 17:17:39 -0700

    test

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
 foodItem = st.text_input("Enter Food Type")
 
@@ -19,10 +18,4 @@ st.write("You selected Food Item: ", myDict.get(option, "Not found"))
 
 st.image('testing.png', caption="testing stuff")
 
-fig = plt.figure() 
-plt.plot([1, 2, 3, 4, 5]) 
 
-st.pyplot(fig)
-
-import sys
-print(sys.path)
