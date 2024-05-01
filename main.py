e9bfb24ecedc63041b5d4c78209661411ca55f50
nicolektang <137560389+nicolektang@users.noreply.github.com>
2024-04-30 17:22:35 -0700

    test

--- main.py
+++ main.py
@@ -10,11 +10,13 @@ st.write("Your chosen food item", foodItem)
 
 myDict = {1: "one", 2: "two", 3: "three", 4: "four"}
 
-optionsList = list(myDict.keys())
+optionsList = list(myDict.values())
 
 option = st.selectbox(
     'What option would you like?',
     optionsList)
+
+selected_key = next(key for key, value in myDict.items() if value == option)
 
 st.write('You selected Series ID:', option)
 st.write("You selected Food Item: ", myDict.get(option, "Not found"))
