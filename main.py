74fe5a5180ee947c24dca5bf2b998a0ad96cf263
nicolektang <137560389+nicolektang@users.noreply.github.com>
2024-04-30 17:19:08 -0700

    test

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import pandas as pd
 import numpy as np
 
 foodItem = st.text_input("Enter Food Type")
+st.write("Your chosen food item", foodItem)
 
 myDict = {1: "one", 2: "two", 3: "three", 4: "four"}
 
 optionsList = list(myDict.keys())
 
 option = st.selectbox(
-    'How would you like to be contacted?',
+    'What option would you like?',
     optionsList)
 
 st.write('You selected Series ID:', option)
