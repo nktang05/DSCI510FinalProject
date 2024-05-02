433289891ebd18af688cc26580d6d2eb2a7114d5
nicolektang <137560389+nicolektang@users.noreply.github.com>
2024-05-01 21:36:27 -0700

    test

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import sqlite3
-import requests
-from bs4 import BeautifulSoup
-import loadData
 
 
 
