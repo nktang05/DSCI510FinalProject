91a43b695a7b39f94a601d946f61f29ac282c13d
nicolektang <137560389+nicolektang@users.noreply.github.com>
2024-05-01 20:41:19 -0700

    test

--- main.py
+++ main.py
@@ -1,28 +1,22 @@
-import sqlite3
 import pandas as pd
 import streamlit as st
+from sqlalchemy import create_engine
 
 # Define the function to feed state data into the database
 def feed_state_data(filename, table_name):
-    # Establish connection to SQLite database
-    conn = sqlite3.connect('tang.db')
-    cur = conn.cursor()
+    # Create SQLAlchemy engine
+    engine = create_engine('sqlite:///tang.db', echo=False)
 
     # Read Excel file into a DataFrame
     data = pd.read_excel(filename)
     data.dropna(inplace=True)
 
     # Write DataFrame to SQLite table
-    data.to_sql(table_name, conn, if_exists='replace', index=False)
+    data.to_sql(table_name, engine, if_exists='replace', index=False)
 
     # Execute a query to fetch some data from the table (for testing)
-    cur.execute(f'SELECT * FROM {table_name} LIMIT 2')
-    return_val = cur.fetchall()
-    print(return_val)
-
-    # Commit changes and close the connection
-    conn.commit()
-    conn.close()
+    query_result = engine.execute(f'SELECT * FROM {table_name} LIMIT 2').fetchall()
+    print(query_result)
 
 # Define the function to get state data
 def get_state_data():
@@ -33,19 +27,16 @@ def get_state_data():
 # Call the function to get state data
 get_state_data()
 
-conn = sqlite3.connect('tang.db', detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
-cur = conn.cursor()
+# Create SQLAlchemy engine
+engine = create_engine('sqlite:///tang.db', echo=False)
 
 ## Your code here
 
-sql_query9 = "SELECT Name From states Where Region = 'West' "
+sql_query9 = "SELECT Name FROM states WHERE Region = 'West'"
 
 # Execute the SQL query and load the results into a DataFrame
-data9 = pd.read_sql(sql_query9, conn)
+data9 = pd.read_sql(sql_query9, engine)
 
 st.write(data9)
 
 print(data9)
-
-conn.commit()
-conn.close()
