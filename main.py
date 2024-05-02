81ddbb85dedd3b2af905f4eb99f19f1f3636adb7
nicolektang <137560389+nicolektang@users.noreply.github.com>
2024-05-01 20:47:48 -0700

    test

--- main.py
+++ main.py
@@ -7,27 +7,3 @@ import matplotlib.pyplot as plt
 
 st.write("hello World")
 
-foodItem = st.text_input("Enter Food Type")
-st.write("Your chosen food item", foodItem)
-
-myDict = {1: "one", 2: "two", 3: "three", 4: "four"}
-
-optionsList = list(myDict.values())
-
-option = st.selectbox(
-    'What option would you like?',
-    optionsList)
-
-selected_key = next(key for key, value in myDict.items() if value == option)
-
-"""
-st.write('You selected Series ID:', selected_key)
-st.write("You selected Food Item: ", option)
-
-st.image('testing.png', caption="testing stuff")
-"""
-
-fig = plt.figure() 
-plt.plot([1, 2, 3, 4, 5]) 
-
-st.pyplot(fig)
