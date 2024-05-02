27082f111d83dbe42a585f5c228ad4fe9454d127
nicolektang <137560389+nicolektang@users.noreply.github.com>
2024-05-01 20:55:55 -0700

    test

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sqlalchemy import create_engine
+import sqlite3
 
 
 
@@ -33,41 +33,45 @@ plt.plot([1, 2, 3, 4, 5])
 
 st.pyplot(fig)
 
-# Define the function to feed state data into the database
-def feed_state_data(filename, table_name):
-    # Create SQLAlchemy engine
-    engine = create_engine('sqlite:///tang.db', echo=False)
-
-    # Read Excel file into a DataFrame
-    data = pd.read_excel(filename)
-    data.dropna(inplace=True)
-
-    # Write DataFrame to SQLite table
-    data.to_sql(table_name, engine, if_exists='replace', index=False)
-
-    # Execute a query to fetch some data from the table (for testing)
-    query_result = engine.execute(f'SELECT * FROM {table_name} LIMIT 2').fetchall()
-    print(query_result)
-
-# Define the function to get state data
+# method to get state and region data as states
 def get_state_data():
-    # Get state data from Excel
+    # Get state data from excel
     statexlsx = "states.xlsx"
+    df1 = pd.read_excel(statexlsx)
+    df1.dropna(inplace=True)
+
+    def feed_state_data(filename, table_name):
+        conn = sqlite3.connect('tang.db', detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+        cur = conn.cursor()
+
+        # make data fram
+        data = pd.read_excel(filename)
+        data.dropna(inplace=True)
+
+        # set to sql table
+        data.to_sql(table_name, conn, if_exists='replace')
+        
+        cur.execute(f'SELECT * FROM {table_name} LIMIT 2')
+        return_val = cur.fetchall()
+        print(return_val)
+
+        conn.commit()
+        conn.close()
+
     feed_state_data(statexlsx, "states")
 
-# Call the function to get state data
-get_state_data()
-
-# Create SQLAlchemy engine
-engine = create_engine('sqlite:///tang.db', echo=False)
+conn = sqlite3.connect('tang.db', detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+cur = conn.cursor()
 
 ## Your code here
 
-sql_query9 = "SELECT Name FROM states WHERE Region = 'West'"
+sql_query9 = "SELECT Name From states Where Region = 'West' "
 
 # Execute the SQL query and load the results into a DataFrame
-data9 = pd.read_sql(sql_query9, engine)
-
-st.write(data9)
+data9 = pd.read_sql(sql_query9, conn)
 
 print(data9)
+st.write(data9)
+
+conn.commit()
+conn.close()
